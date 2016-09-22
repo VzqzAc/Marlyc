a8f09805913834ffeabd41a3e7ea0b470a10c137
Omar Vazquez <omar.vazquezac@gmail.com>
2016-09-22 16:55:28 -0500

    started basic db model with TDD

---
+++ express-app/spec/models/baseSpec.js
@@ -0,0 +1,26 @@
+var Model = require('../models/Base'), dbMockup = {};
+
+
+describe('Models', () => {
+    
+    it('should create a new model', () => {
+        var model = new Model(dbMockup);
+        expect(model.db).toBeDefined();
+        expect(model.extend).toBeDefined();
+    });
+
+    
+    it('should be extendable', (next) => {
+        var model = new Model(dbMockup);
+        var OtherTypeOfModel = model.extend({
+            myCustomModelMethod: () => { }
+        });
+        var model2 = new OtherTypeOfModel(dbMockup);
+        expect(model2.db).toBeDefined();
+        expect(model2.myCustomModelMethod).toBeDefined();
+        next();
+    });
+        
+        
+});
+    
