ecfa827ac2b4b41eff3898f9badc2fa2ee2560cc
Omar Vazquez <omar.vazquezac@gmail.com>
2016-09-25 18:34:15 -0500

    ignoring extra instances

--- express-app/app.js
+++ express-app/app.js
@@ -11,7 +11,7 @@ var users = require('./controllers/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'templates'));
+app.set('views', path.join(__dirname, '/templates'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
